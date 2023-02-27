73afa7b17f0a16528ca704930c48edbad377fbd2
Mehmet Ali <58392243+mehmetalikilicli@users.noreply.github.com>
2023-02-27 15:38:46 +0300

    App Created

---
+++ SurucuEhliyeti/SurucuEhliyeti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurucuEhliyeti
+//
+//  Created by Mehmet Ali Kılıçlı on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
