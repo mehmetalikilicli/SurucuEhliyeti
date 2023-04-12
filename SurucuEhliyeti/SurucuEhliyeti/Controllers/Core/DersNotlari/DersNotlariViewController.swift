60ddea67fdbba4169ebf412ca54001d25b1924e1
Mehmet Ali <58392243+mehmetalikilicli@users.noreply.github.com>
2023-04-12 14:05:02 +0300

    AGPI CollectionView UI

--- SurucuEhliyeti/SurucuEhliyeti/Controllers/Core/DersNotlari/DersNotlariViewController.swift
+++ SurucuEhliyeti/SurucuEhliyeti/Controllers/Core/DersNotlari/DersNotlariViewController.swift
@@ -8,20 +8,27 @@
 import UIKit
 
 class DersNotlariViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func trafikIsaretleriButton(_ sender: Any) {
+        let trafikIsaretleriVC = TrafikIsaretleriViewController()
+        self.navigationController?.pushViewController(trafikIsaretleriVC, animated: true)
+        
         print("trafikIsaretleriButton  tiklandi.")
     }
     
     @IBAction func polisIsaretleriButton(_ sender: Any) {
+        let polisIsaretleriVC = PolisIsaretleriViewController()
+        self.navigationController?.pushViewController(polisIsaretleriVC, animated: true)
         print("polisIsaretleriButton  tiklandi.")
     }
     
     @IBAction func AGPIButton(_ sender: Any) {
+        let aracGostergePaneliVC = AGPIViewController()
+        self.navigationController?.pushViewController(aracGostergePaneliVC, animated: true)
         print("AGPIButton tiklandi.")
     }
     
--- SurucuEhliyeti/SurucuEhliyeti/View/DersNotlari/PolisIsaretleri/PolisIsaretleriViewController.swift
+++ SurucuEhliyeti/SurucuEhliyeti/View/DersNotlari/PolisIsaretleri/PolisIsaretleriViewController.swift
@@ -11,6 +11,5 @@ class PolisIsaretleriViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 }
