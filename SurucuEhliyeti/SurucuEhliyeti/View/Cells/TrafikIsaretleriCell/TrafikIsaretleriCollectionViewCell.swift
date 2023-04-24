d93d75a853f58ec0fcfef94a5fe52e9b189e252a
Mehmet Ali <58392243+mehmetalikilicli@users.noreply.github.com>
2023-04-24 18:03:59 +0300

    Finish popUp For AGPI

--- SurucuEhliyeti/SurucuEhliyeti/View/Cells/TrafikIsaretleriCell/TrafikIsaretleriCollectionViewCell.swift
+++ SurucuEhliyeti/SurucuEhliyeti/View/Cells/TrafikIsaretleriCell/TrafikIsaretleriCollectionViewCell.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class TrafikIsaretleriCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var TILabel: UILabel!
+    @IBOutlet weak var trafikIsaretImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setUpCell(trafikIsaret: TrafikIsaret){
+        trafikIsaretImageView.image = UIImage(named: "\(trafikIsaret.trafikIsaret)")
     }
 
 }
