4e6ec874c08561386c60336624af33a8425a86e2
Ahsen Bahtışen <65489789+ahsen-bahtisen@users.noreply.github.com>
2023-05-08 12:40:57 +0300

    Dynamic CollectionView

--- SurucuEhliyeti/SurucuEhliyeti/View/DersNotlari/AGPI/AGPIViewController.swift
+++ SurucuEhliyeti/SurucuEhliyeti/View/DersNotlari/AGPI/AGPIViewController.swift
@@ -65,3 +65,29 @@ extension AGPIViewController : UICollectionViewDelegate, UICollectionViewDataSou
         isPopupOpen = false
     }
 }
+
+extension AGPIViewController : UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let bounds = collectionView.bounds
+        let heightVal = self.view.frame.height
+        let widthVal = self.view.frame.width
+        let cellSize = (heightVal < widthVal) ? bounds.height / 3 : bounds.width / 3
+        
+        return CGSize(width: cellSize - 10, height: cellSize - 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        10
+    }
+    
+}
