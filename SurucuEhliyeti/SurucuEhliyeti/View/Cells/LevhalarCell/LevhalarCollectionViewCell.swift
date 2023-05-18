d1fc13723816e17bec32817a76b57975cb82d611
Ahsen Bahtışen <65489789+ahsen-bahtisen@users.noreply.github.com>
2023-05-18 23:53:55 +0300

    added levhalar files

---
+++ SurucuEhliyeti/SurucuEhliyeti/View/Cells/LevhalarCell/LevhalarCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LevhalarCollectionViewCell.swift
+//  SurucuEhliyeti
+//
+//  Created by Ahsen Bahtışen on 18.05.2023.
+//
+
+import UIKit
+
+class LevhalarCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SurucuEhliyeti/SurucuEhliyeti/View/DersNotlari/Levhalar/LevhalarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LevhalarViewController.swift
+//  SurucuEhliyeti
+//
+//  Created by Ahsen Bahtışen on 18.05.2023.
+//
+
+import UIKit
+
+class LevhalarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
